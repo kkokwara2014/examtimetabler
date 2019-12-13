a5769c6a03a768458b603f649ba0b925732b946a
kkokwara2014 <kkokwara2014@gmail.com>
2019-12-13 03:01:39 +0100

    --- Auto-Git Commit ---

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'lastname','firstname', 'email','phone', 'password','role_id',
     ];
 
     /**
